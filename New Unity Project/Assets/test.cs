a30ab8d11faa21b9897fa4924ca3cf89d04da5c2
mohammedalwayel <70606401+mohammedalwayel@users.noreply.github.com>
2022-08-03 16:06:16 +0300

    11

---
+++ New Unity Project/Assets/test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //متغيرات 
+
+    string heroName = "ali";
+
+    string villainName = "joj";
+    int heroAge = 20; 
+    int  heroHeight = 177;
+    int heroSuperPower = 100;
+    int vililanHeight = 250;
+    int villainAge = 100;
+    int villainSuperPower = 1000;
+    int ageDifference = heroAge - villainAge; 
+    void Start()
+    {
+        
+        print(heroAge);
+        print("my name is"+ heroName);
+        print(heroHeight);
+        print(heroSuperPower);
+
+       
+
+
+    }
+
+    // Update is called once per frame
+ 
+        
+    
+}
